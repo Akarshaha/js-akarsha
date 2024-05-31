6a6499be19ed0b100475f6b3aa6150b4236d0f25
Akarshaha <61219561+Akarshaha@users.noreply.github.com>
2024-05-31 10:23:51 +0000

    numbers and math

---
+++ .devcontainer/basics/numbers.js
@@ -0,0 +1,17 @@
+// const score = 100;
+// console.log(score.typeof);
+// console.log(score.toString());
+// console.log(score.typeof);
+// const balance = new Number(100);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// const otherScore = 1121.899;
+// console.log(otherScore.toPrecision(2));
+// const cents = 1000000;
+// console.log(cents.toLocaleString('en-IN'));
+
+console.log(Math);
+console.log(Math.ceil(6.1));
+const max = 10;
+const min = 20;
+console.log((Math.ceil(Math.random() * (max - min + 1) + min)));
