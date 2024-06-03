91abe2e1712e7be88530373c7697cdd2a62de1b1
Akarshaha <61219561+Akarshaha@users.noreply.github.com>
2024-06-03 20:42:01 +0000

    FUNCTIONS

--- .devcontainer/functions/functions.js
+++ .devcontainer/functions/functions.js
@@ -25,4 +25,40 @@ function loginusermessage(username) {  //we can also set username as default by 
     }
     return `${username} just logged in`
 } 
-console.log(loginusermessage("ashik"))
+// console.log(loginusermessage("ashik"))
+// function calculatecartprice(val1,val2, ...item) {    //we can use spread operator(...) when taking more inputs
+    // return item;                                      it will store in arrays
+    
+}
+// console.log(calculatecartprice(11,100,200,340));
+
+
+const user = {
+    product: "car",
+    price:5000
+}
+
+
+// function handleobject(anyobject) {                //syntax and example for using function for objects
+    // console.log(`product is ${anyobject.product} and price is ${anyobject.price}`);
+    
+// }
+ handleobject(user)
+// handleobject({                we can also add/create object while calling the function only
+//     product: "bike",
+//     price: 2500
+// })
+// const aka = ['a','b','c','d']
+// const mynewarray = [200,23,34,45,67]
+
+
+// function getarray(my2val) {               syntax and example for using functions for arrays(IMP)
+//     return my2val[2];
+    
+// }
+console.log(getarray(mynewarray));
+console.log(getarray(aka));
+
+console.log(getarray([
+    1,2,3,4,5,6
+]))
